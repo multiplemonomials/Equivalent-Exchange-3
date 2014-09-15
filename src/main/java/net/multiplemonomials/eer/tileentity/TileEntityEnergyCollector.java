4e825e8abdc2f19ee3ef3beb61c84d7df59d725d
multiplemonomials <jsmith@crackofdawn.onmicrosoft.com>
2014-09-14 21:50:17 -0700

    Fix a few bugs in last commit

--- src/main/java/net/multiplemonomials/eer/tileentity/TileEntityEnergyCollector.java
+++ src/main/java/net/multiplemonomials/eer/tileentity/TileEntityEnergyCollector.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.Packet;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
@@ -67,6 +68,18 @@ public class TileEntityEnergyCollector extends TileEntityEE
        leftoverEMC = nbtTagCompound.getDouble("leftoverEMC");
        
        upgradeLevel = nbtTagCompound.getByte("level");
+       
+       // Read in the ItemStacks in the inventory from NBT
+       NBTTagList tagList = nbtTagCompound.getTagList("Items", 10);
+       for (int i = 0; i < tagList.tagCount(); ++i)
+       {
+           NBTTagCompound tagCompound = tagList.getCompoundTagAt(i);
+           byte slotIndex = tagCompound.getByte("Slot");
+           if (slotIndex >= 0 && slotIndex < inventory.length)
+           {
+               inventory[slotIndex] = ItemStack.loadItemStackFromNBT(tagCompound);
+           }
+       }
     }
     
     @Override
@@ -77,6 +90,21 @@ public class TileEntityEnergyCollector extends TileEntityEE
        nbtTagCompound.setDouble("leftoverEMC", leftoverEMC);
        
        nbtTagCompound.setByte("level", upgradeLevel);
+       
+       // Write the ItemStacks in the inventory to NBT
+       NBTTagList tagList = new NBTTagList();
+       for (int currentIndex = 0; currentIndex < inventory.length; ++currentIndex)
+       {
+           if (inventory[currentIndex] != null)
+           {
+               NBTTagCompound tagCompound = new NBTTagCompound();
+               tagCompound.setByte("Slot", (byte) currentIndex);
+               inventory[currentIndex].writeToNBT(tagCompound);
+               tagList.appendTag(tagCompound);
+           }
+       }
+       
+       nbtTagCompound.setTag("Items", tagList);
     }
     
     @Override
@@ -108,7 +136,7 @@ public class TileEntityEnergyCollector extends TileEntityEE
     	
     	if(leftoverEMC < getMaxStorableEMC())
     	{
-    		leftoverEMC += (CommonConfiguration.ENERGY_COLLECTOR_EMC_PER_TICK[upgradeLevel - 1] * (lightLevel / 15.0));
+    		leftoverEMC += (CommonConfiguration.ENERGY_COLLECTOR_EMC_PER_TICK[upgradeLevel - 1] * (lightLevel / 14.0));
     	}
     	
     	drainEMCToKleinStar();
