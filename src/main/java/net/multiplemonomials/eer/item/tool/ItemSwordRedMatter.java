ae2d1f50d8adb05d41a90dbc02599598f5a797df
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2014-12-24 19:19:45 -0700

    Bugfix Red Matter Sword

--- src/main/java/net/multiplemonomials/eer/item/tool/ItemSwordRedMatter.java
+++ src/main/java/net/multiplemonomials/eer/item/tool/ItemSwordRedMatter.java
@@ -1,12 +1,17 @@
 package net.multiplemonomials.eer.item.tool;
 
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import net.multiplemonomials.eer.configuration.CommonConfiguration;
 import net.multiplemonomials.eer.interfaces.IStoresEMC;
 import net.multiplemonomials.eer.reference.Names;
+import net.multiplemonomials.eer.util.EMCHelper;
 
 public class ItemSwordRedMatter extends ItemSwordDarkMatter implements IStoresEMC {
 
@@ -100,15 +105,76 @@ public class ItemSwordRedMatter extends ItemSwordDarkMatter implements IStoresEM
 		return false;
 	}
 	
-	@Override
-	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float x2, float y2, float z2)
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player)
     {
-        return false;
+    	verifyItemStackHasNBTTag(itemStack);
+    	
+    	
+    	int damageToDo = getDamage(itemStack) == CommonConfiguration.MAX_ITEM_CHARGES ? 2 : ((CommonConfiguration.MAX_ITEM_CHARGES - getDamage(itemStack) + 1) * 2);
+		itemStack.stackTagCompound.setInteger("damageToDo", damageToDo);
+
+		if(!player.capabilities.isCreativeMode)
+		{
+			//ensure enough EMC is available
+	    	double emcLeft = getAvailableEMC(itemStack);
+	    	double neededEMC = CommonConfiguration.RM_HOE_REQUIRED_EMC_PER_BLOCK * damageToDo;
+
+	    	if(emcLeft < neededEMC)
+	    	{
+	    		emcLeft += EMCHelper.consumeEMCFromPlayerInventory(player, neededEMC - emcLeft);
+	    	}
+	    	
+    		itemStack.stackTagCompound.setDouble(Names.NBT.EMC_STORED, emcLeft);
+	    	
+	    	if(emcLeft < neededEMC)
+	    	{
+	    		return itemStack;
+	    	}
+		}
+    	
+    	
+    	player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
+    	
+
+        return itemStack;
     }
 	
     public EnumAction getItemUseAction(ItemStack itemStack)
     {
         return EnumAction.bow;
+    }
+    
+    /**
+     * How long it takes to use or consume an item
+     */
+    public int getMaxItemUseDuration(ItemStack p_77626_1_)
+    {
+        return 5000;
+    }
+    
+    /**
+     * called when the player releases the use item button. Args: itemstack, world, entityplayer, itemInUseCount
+     */
+    public void onPlayerStoppedUsing(ItemStack itemStack, World world, EntityPlayer player, int count)
+    {
+        if(count > 4500)
+        {
+        	int damageToDo = itemStack.stackTagCompound.getInteger("damageToDo");
+	    	double neededEMC = CommonConfiguration.RM_HOE_REQUIRED_EMC_PER_BLOCK * damageToDo;
+	    	
+	    	tryTakeEMC(itemStack, neededEMC); //we checked this earlier, so there should be EMC
+	    	
+	    	//now, actually DO something
+	    	AxisAlignedBB pushBox = AxisAlignedBB.getBoundingBox(player.posX - 5, player.posY - 2, player.posZ - 5, player.posX + 5, player.posY + 1, player.posZ + 5);
+			for(Object object : player.worldObj.getEntitiesWithinAABB(EntityMob.class, pushBox))
+			{
+				EntityMob mob = (EntityMob)object;
+				
+				mob.attackEntityFrom(DamageSource.causePlayerDamage(player), damageToDo);
+			}
+        }
+        
+        
     }
 
 }
